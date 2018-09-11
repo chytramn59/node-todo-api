2f2f398b4b747b97cb8e39915f509ab76dca4e44
chytramn59 <cnagaraju@qwinix.io>
2018-09-11 11:50:09 +0530

    GET method

--- server/server.js
+++ server/server.js
@@ -22,6 +22,13 @@ app.post('/todos',(req,res)=>{
 
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+    res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
+    })
+})
 app.post('/user',(req,res)=>{
     var users = new User({
         email:req.body.email
