7ebd86afd76dfdb421387fbd0705c0d510bfc95b
chytramn59 <cnagaraju@qwinix.io>
2018-09-17 13:16:43 +0530

    herok

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -56,8 +57,8 @@ app.get('/todos/:id',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server is up ');
+app.listen(port,()=>{
+    console.log(`server is up ${port}`);
 })
 
 
