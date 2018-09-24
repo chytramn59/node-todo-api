507e6f9311d150c4aa1e02cc597890c9102e57f3
chytramn59 <cnagaraju@qwinix.io>
2018-09-24 10:18:55 +0530

    delete/route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,6 +3,10 @@ const {Todo} = require('./../server/models/todo');
 const {ObjectID} = require('mongodb');
 const {User} = ('./../server/models/user');
 
-Todo.remove({}).then((result) =>{
-    console.log(result);
+// Todo.remove({}).then((result) =>{
+//     console.log(result);
+// });
+
+Todo.findByIdAndRemove('5ba86787b3a7b0543a930a1d').then((todo)=>{
+    console.log(todo);
 });
