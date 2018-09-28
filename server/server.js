4d443f328862f597566983d37c1d0d57d61c8205
chytramn59 <cnagaraju@qwinix.io>
2018-09-28 15:36:48 +0530

    user route

--- server/server.js
+++ server/server.js
@@ -97,7 +97,15 @@ app.patch('/todos/:id',(req,res)=>{
         res.status(400).send();
     })
 })
-
+app.post('/users',(req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+    user.save().then((user)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+});
 app.listen(port,()=>{
     console.log(`server is up ${port}`);
 })
