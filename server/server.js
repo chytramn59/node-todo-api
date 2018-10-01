f708492769ab288fd8e3e74e9d264443a456a8c7
chytramn59 <cnagaraju@qwinix.io>
2018-10-01 16:11:53 +0530

    userSchema

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var {Todo} = require('./models/todo');
 
 
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ;
 
 app.use(bodyParser.json());
 
@@ -100,12 +100,16 @@ app.patch('/todos/:id',(req,res)=>{
 app.post('/users',(req,res)=>{
     var body = _.pick(req.body,['email','password']);
     var user = new User(body);
-    user.save().then((user)=>{
-        res.send(user);
+    user.save().then(()=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
     }).catch((e)=>{
         res.status(400).send(e);
     })
 });
+
+
 app.listen(port,()=>{
     console.log(`server is up ${port}`);
 })
