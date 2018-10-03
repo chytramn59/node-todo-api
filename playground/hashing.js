74bf21f8fea0fbe74a097fb7eda453b4875db601
chytramn59 <cnagaraju@qwinix.io>
2018-10-03 17:26:45 +0530

    hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
  
-var data = {
-    id:10
-};
-var token = jwt.sign(data,'123abc');
-    console.log(token);
-var decoded = jwt.verify(token,'123abc');
-    console.log('decoded',decoded);
+var password = "123abc!";
+
+// bcrypt.genSalt((10),(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash);
+//     })
+// });
+
+var hashedPassword = "$2a$10$Z5n2jT63.R6kFabv9x8vIe76Xz9Bc8ckRr8M3bJ6KnOWf4In6.ky6";
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+})
+
+// var data = {
+//     id:10
+// };
+// var token = jwt.sign(data,'123abc');
+//     console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+//     console.log('decoded',decoded);
+
+
+
 // var message = 'i am user 3';
 // var hash = SHA256(message).toString();
 
